fb5d4ffeddc302a9bb09b79b05457e772ee37bbb
Naisalie <naisaliew@gmail.com>
2023-09-21 20:59:49 -0400

    Create function that calls math functions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Basic math functions
 const add = (a, b) => {
   return a + b;
 };
@@ -11,4 +12,29 @@ const multiply = (a, b) => {
 };
 const divide = (a, b) => {
   return a / b;
+};
+
+//Variables for each part of calc operation
+let num1;
+let operateSymbol;
+let num2;
+
+//Operate function that takes operator and 2 numbers and calls one of the basic math functions on them
+const operate = (firstNum, operator, secondNum) => {
+  switch (operator) {
+    case "+":
+      add(firstNum, secondNum);
+      break;
+    case "-":
+      subtract(firstNum, secondNum);
+      break;
+    case "*":
+      multiply(firstNum, secondNum);
+      break;
+    case "/":
+      divide(firstNum, secondNum);
+      break;
+    default:
+      break;
+  }
 };
