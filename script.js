8f24d8103a8afe4d3f1e0d114caca825fbf2e14e
Naisalie <naisaliew@gmail.com>
2023-10-04 01:23:44 -0400

    Add decimal functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const sortInput = (input) => {
-  !isNaN(input) ? displayInput(input) : isSymbol(input);
+  !isNaN(input) || input == "." ? displayInput(input) : isSymbol(input);
 };
 
 //Variable storage
