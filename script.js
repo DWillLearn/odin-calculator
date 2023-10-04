7445392b8308e541aad130d6995202fb527415dd
Naisalie <naisaliew@gmail.com>
2023-10-03 21:10:39 -0400

    Condense and separate code where needed

--- script.js
+++ script.js
@@ -1,111 +1,68 @@
-//Basic math functions
-const add = (a, b) => a + b;
-
-const subtract = (a, b) => a - b;
-
-const multiply = (a, b) => a * b;
-
-const divide = (a, b) => (a == "0" || b == "0" ? ">:(" : a / b);
-
-//Variables for each part of calc operation
-let num1;
-let operateSymbol;
-let num2;
-let answer;
-
-//Operate function that takes operator and 2 numbers and calls one of the basic math functions on them
-const operate = (firstNum, operator, secondNum) => {
-  switch (operator) {
-    case "+":
-      answer = add(firstNum, secondNum);
-      break;
-    case "-":
-      answer = subtract(firstNum, secondNum);
-      break;
-    case "*":
-    case "×":
-      answer = multiply(firstNum, secondNum);
-      break;
-    case "/":
-    case "÷":
-      answer = divide(firstNum, secondNum);
-      break;
-    default:
-      break;
-  }
-};
-
-//Variable that stores display value
-let displayNum = "";
-let showOnScreen;
-
-//The display itself
-let calcScreen = document.querySelector(".screen--text");
-
-//All the calculator buttons
-let calcButtons = document.querySelector(".interface--buttons");
-let allBtn = calcButtons.querySelectorAll(".row--button");
-
-//Functions that populate calculator display based on keyboard input or mouse input
-const keyInput = () => {
+//Send keyboard or mouse input to calculator
+const keyInput = (arr) => {
   document.addEventListener("keyup", (e) => {
-    allBtn.forEach((btn) => {
-      switch (e.key) {
-        case btn.innerText.toLowerCase():
-        case "/":
-        case "*":
-        case "Enter":
-          displayCalc(e.key);
-          break;
-        case "Backspace":
-          displayCalc("←");
-          break;
-        default:
-          break;
-      }
+    arr.forEach((btn) => {
+      let input = btn.innerText;
+      e.key === btn.innerText && !isNaN(input) ? displayInput(input) : isSymbol(input);
     });
   });
 };
 
-const mouseInput = () => {
-  allBtn.forEach((btn) => {
-    btn.addEventListener("click", (e) => displayCalc(e.target.innerText));
+const mouseInput = (arr) => {
+  arr.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let input = btn.innerText;
+      !isNaN(input) ? displayInput(input) : isSymbol(input);
+    });
   });
 };
 
-const displayCalc = (button) => {
-  let input = button;
-  isNaN(input) ? (showOnScreen = false) : (showOnScreen = true);
-  showOnScreen ? isNum(input) : isSymbol(input);
-  calcScreen.innerText = displayNum;
-};
+//Variable storage
+let num1;
+let operateSymbol;
+let num2;
 
-const isNum = (num) => {
-  num === displayNum ? (displayNum = num) : (displayNum += num); //fix this
-  !operateSymbol ? (num1 = parseInt(displayNum)) : (num2 = parseInt(displayNum));
+//Calculator screen
+let calcScreen = document.querySelector(".screen--text");
+
+//Populate calculator screen with received input if it is a number, then send off to storage variable
+const displayInput = (num) => {
+  calcScreen.innerText += num;
+  console.log(num1, operateSymbol, num2);
 };
 
 const isSymbol = (symbol) => {
-  if (symbol != "←") displayNum = "";
-  if (symbol != "=" && symbol != "C" && symbol != "←" && !operateSymbol) operateSymbol = symbol;
+  let screenNums;
   switch (symbol) {
-    case "C":
-      clear();
-      break;
-    case "←":
-      if (displayNum === answer) return;
-      displayNum.slice(0, -1);
-
+    case "+":
+    case "-":
+    case "*":
+    case "×":
+    case "/":
+    case "÷":
+      if (!operateSymbol) operateSymbol = symbol;
+      screenNums = calcScreen.innerText;
+      if (!num1) num1 = screenNums;
+      calcScreen.innerText = "";
       break;
     case "Enter":
     case "=":
+      screenNums = calcScreen.innerText;
+      if (!num2) num2 = screenNums;
       operate(num1, operateSymbol, num2);
-      displayNum = answer;
       break;
+    // case "Backspace":
+    // case "←":
+    //     break;
+    // case "C":
+    //   break;
+    //   default:
+    //     break;
   }
+  console.log(num1, operateSymbol, num2);
 };
 
-const clear = () => {
+const clear2 = () => {
   num1 = undefined;
   operateSymbol = undefined;
   num2 = undefined;
@@ -115,7 +72,41 @@ const clear = () => {
 //Add decimals, but not more than 1
 
 //Run initial functions on load
-window.addEventListener("load", (e) => {
-  keyInput();
-  mouseInput();
+window.addEventListener("load", () => {
+  let allBtn = document.querySelectorAll(".row--button");
+  keyInput(allBtn);
+  mouseInput(allBtn);
 });
+
+//Basic math functions
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => (a == "0" || b == "0" ? ">:(" : a / b);
+
+//Operate function that takes operator and 2 numbers and calls one of the basic math functions on them
+const operate = (a, operator, b) => {
+  let answer;
+  switch (operator) {
+    case "+":
+      answer = a + b;
+      break;
+    case "-":
+      answer = a - b;
+      break;
+    case "*":
+    case "×":
+      answer = a * b;
+      break;
+    case "/":
+    case "÷":
+      a == "0" || b == "0" ? (answer = ">:(") : (answer = a / b);
+      break;
+    default:
+      break;
+  }
+  calcScreen.innerText = answer;
+};
